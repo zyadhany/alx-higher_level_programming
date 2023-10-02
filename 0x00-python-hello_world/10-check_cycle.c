41d5128873240c61829e44f9bc5fcb6138d470ab
zyadhany <zeyad.hany2003@gmail.com>
2023-10-03 01:01:12 +0300

    added task

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check for loop in LL
+ * @list: head of linked list
+ *
+ * Description - check for loops in LL
+ * Return: 1 if cycled, 0 if not
+ */
+
+int check_cycle(listint_t *list)
+{
+    if (!list)
+    {
+        return (0);
+    }
+    
+	return (0);
+}
