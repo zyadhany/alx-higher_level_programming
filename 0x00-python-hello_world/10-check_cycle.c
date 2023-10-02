7e953a6cac38f4deea2685f9f6dc2990629df81c
zyadhany <zeyad.hany2003@gmail.com>
2023-10-03 01:39:08 +0300

    added task

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,9 +10,18 @@
 
 int check_cycle(listint_t *list)
 {
+    listint_t *fast = list, *slow = list;
+
     if (!list)
-    {
         return (0);
+
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (fast == slow)
+            return (1);
     }
     
 	return (0);
