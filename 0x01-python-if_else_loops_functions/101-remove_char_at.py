a46d0719699fc8a125f36ce576107a2befd78b66
zyadhany <zeyad.hany2003@gmail.com>
2023-10-03 18:43:09 +0300

    added task

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-def remove_char_at(str, n):
-    if n < 0 or n >= len(str):
-        return str
-    s = str[0:n] + str[n + 1:]
-    return s
+def magic_calculation(a, b, c):
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    else:
+        return (a * b - c)
