6659bf7f3d18efe544f67e0419b17e04a9e9d3d1
zyadhany <zeyad.hany2003@gmail.com>
2023-10-05 18:25:54 +0300

    added task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(n))
 
-    for i in range(1, n):
-        print("{}: {}".format(i, sys.argv[i + 1]))
+    for i in range(1, n + 1):
+        print("{}: {}".format(i, sys.argv[i]))
