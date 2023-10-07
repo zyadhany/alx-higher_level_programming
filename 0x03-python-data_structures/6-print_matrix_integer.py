1df684fce7f915c1d8f22b0f51090ce0af4178b0
zyadhany <zeyad.hany2003@gmail.com>
2023-10-08 01:52:42 +0300

    added task

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        return
-
     for i in matrix:
         for j in i:
             print("{:d}".format(j), end=" ")
