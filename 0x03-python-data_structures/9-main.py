f80476bfd8ef30d7ac920107c45ecfcb6f2b59aa
zyadhany <zeyad.hany2003@gmail.com>
2023-10-08 02:15:59 +0300

    added task

--- 0x03-python-data_structures/9-main.py
+++ 0x03-python-data_structures/9-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-element_at = __import__('1-element_at').element_at
+max_integer = __import__('9-max_integer').max_integer
 
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+my_list = [1, 90, 2, 13, 34, 5, -13, 3]
+max_value = max_integer(my_list)
+print("Max: {}".format(max_value))
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    
-    print("{:d}".format(i))
+
+    if not my_list:
+        return None
+
+    if not len(my_list):
+        return (None)
+
+    mx = 0
+
+    for i in my_list:
+        mx = max(mx, i)
+    return (mx)
