0c608d49650a5cfb878ca9bc2e22c877d83aa9b9
zyadhany <zeyad.hany2003@gmail.com>
2023-10-08 01:50:08 +0300

    added task

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
 
-    if not my_list:
-        return
-
-    n = len(my_list) - 1
-    while n >= 0:
-        print("{:d}".format(my_list[n]))
-        n -= 1
+    def print_matrix_integer(matrix=[[]]):
