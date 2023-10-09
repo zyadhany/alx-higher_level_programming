bc18c5730d7af99766041c42afc6896bcca910f8
zyadhany <zeyad.hany2003@gmail.com>
2023-10-09 17:15:56 +0300

    added task

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,4 +3,5 @@ def divisible_by_2(my_list=[]):
     X = []
     for n in my_list:
         X.append(not n % 2)
+    my_list = X
     return (X)
--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,5 +4,6 @@ def delete_at(my_list=[], idx=0):
 
     if idx < 0 or idx >= n:
         return my_list
-    X = my_list[0:idx] + my_list[idx + 1:]
-    return (X)
+
+    del my_list[idx]
+    return (my_list)
