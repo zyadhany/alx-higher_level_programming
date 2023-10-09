dba3fef89baea09b427085d018aa7c9f91758d0d
zyadhany <zeyad.hany2003@gmail.com>
2023-10-09 17:12:11 +0300

    added task

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    n = len(my_list)
+    
+    if idx < 0 or idx >= n:
+        return my_list
+    
+    return my_list[0:idx] + my_list[idx + 1:]
