40b8a1f6115888224ddc3ed18efff6a678bc3d0e
zyadhany <zeyad.hany2003@gmail.com>
2023-10-11 23:10:00 +0300

    added task

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-best_score = __import__('10-best_score').best_score
+def best_score(a_dictionary={}):
+    if not a_dictionary:
+        return (None)
 
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
-best_key = best_score(None)
-print("Best score: {}".format(best_key))
+    mx = 0
+    for key, value in a_dictionary.items():
+        mx = max(mx, value)
+    return (mx)
