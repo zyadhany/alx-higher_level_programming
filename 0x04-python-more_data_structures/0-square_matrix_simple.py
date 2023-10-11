39b2d8b0c8d42183cc3ffb91a087d83e5a2ac5b1
zyadhany <zeyad.hany2003@gmail.com>
2023-10-11 11:25:10 +0300

    added task

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=None):
+    if not matrix:
+        return matrix
+
+    n = len(matrix)
+    m = len(matrix[0])
+
+    new_matrix = [row[:] for row in matrix]
+
+    for i in range(n):
+        for j in range(m):
+            new_matrix[i][j] *= new_matrix[i][j]
+    
+    return new_matrix
