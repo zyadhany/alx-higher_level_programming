d580d9063bb89a5af479b87e18b091b93fc9c30f
zyadhany <zeyad.hany2003@gmail.com>
2023-10-11 23:37:51 +0300

    added task

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for n in a_dictionary:
-        if a_dictionary[n] == value:
-            del a_dictionary[n]
+    for key, val in a_dictionary.items():
+        if val == value:
+            del a_dictionary[key]
     return (a_dictionary)
