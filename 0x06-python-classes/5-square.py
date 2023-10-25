5b31b4523aa558555c1c60d93f46dba14b04bcfb
zyadhany <zeyad.hany2003@gmail.com>
2023-10-25 04:48:48 +0300

    added task

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,25 +1,38 @@
 #!/usr/bin/python3
+
+"""class square."""
+
+
 class Square:
+    """square."""
+
     def __init__(self, size=0):
+        """ new square """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.size = size
 
+    @property
     def size(self):
-        return (self.size)
+        """get value"""
+        return (self.__size)
 
+    @size.setter
     def size(self, value):
+        """ setvalue """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.size = value
+        self.__size = value
 
     def area(self):
-        return self.size ** 2
+        """ square area """
+        return (self.__size ** 2)
 
     def my_print(self):
+        """print square"""
         for i in range(self.size):
             print("#" * self.size)
