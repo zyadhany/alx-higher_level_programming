37a64a61a0df3d0a27b6ebdcb5724b29abde6df3
zyadhany <zeyad.hany2003@gmail.com>
2023-10-25 04:43:02 +0300

    added task

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
+
+"""class square."""
+
+
 class Square:
+    """square."""
+
     def __init__(self, size=0):
+        """ new square """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.size = size
+        self.__size = size
 
     def size(self):
-        return (self.size)
+        """get value"""
+        return (self.__size)
 
     def size(self, value):
+        """ setvalue """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.size = value
+        self.__size = value
 
     def area(self):
-        return self.size ** 2
+        """ square area """
+        return (self.__size ** 2)
