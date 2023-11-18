cd28ad77c578668eaa60cba0b0bfcc988a719be4
zyadhany <zeyad.hany2003@gmail.com>
2023-11-18 11:22:40 +0200

    added task

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+"""class Rectangle."""
+
+
+class Rectangle:
+    """Rectangle."""
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        if not self.__width or not self.height:
+            return (0)
+        return ((self.__width + self.__height) * 2)
+
+    def __str__(self):
+        if not self.__width or not self.height:
+            return ("")
+
+        s = ""
+        for i in range(self.__height):
+            s += "#" * self.__width + "\n"
+        return (s[:-1])
+
+    def __repr__(self):
+        return ("Rectangle({}, {})".format(self.__width, self.height))
+
+    def __del__(self):
+        print("Bye rectangle...")
+
+    pass
