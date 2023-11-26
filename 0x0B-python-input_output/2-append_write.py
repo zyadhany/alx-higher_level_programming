f629ce7cb3f6a98181e18d2088dc32fbc78c1336
zyadhany <zeyad.hany2003@gmail.com>
2023-11-26 19:19:19 +0200

    added task

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -7,5 +7,5 @@
 def append_write(filename="", text=""):
     """apped text to file"""
 
-    with open(filename, "w", encoding="utf-8") as fn:
+    with open(filename, "a", encoding="utf-8") as fn:
         return fn.write(text)
