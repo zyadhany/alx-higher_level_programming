b7b7c7af4ebf849ccb867723bfe7089060ccbde7
zyadhany <zeyad.hany2003@gmail.com>
2023-11-26 19:13:44 +0200

    added task

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,4 +7,4 @@ def write_file(filename="", text=""):
     """Documentation of function"""
 
     with open(filename, "w", encoding="utf-8") as fn:
-        fn.write(text)
+        return fn.write(text)
