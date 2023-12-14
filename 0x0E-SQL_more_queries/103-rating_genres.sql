216bbc8d3e21e622c3e74d7e6c185ad92a580a9a
zyadhany <zeyad.hany2003@gmail.com>
2023-12-14 04:58:23 +0200

    added task 9

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,5 +1,7 @@
 -- Get all DB of server.
-SELECT state, MAX(value) AS max_temp
-FROM temperatures
-GROUP BY state
-ORDER BY state;
+
+SELECT name, SUM(tv_show_ratings.rate) 'rating' FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY name
+ORDER BY rating DESC;
