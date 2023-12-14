7ba1d156fc14329ae6b65e95571e7c6d8d4b7254
zyadhany <zeyad.hany2003@gmail.com>
2023-12-14 04:38:18 +0200

    added task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,12 +1,4 @@
 -- Get all DB of server.
--- Get all DB of server.
-CREATE TABLE IF NOT EXISTS `second_table`(
-    `id` INT,
-    `name` VARCHAR(256),
-    `score` INT
-    );
-
-INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
-INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
-INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
-INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
+SELECT cities.id, cities.name, states.name FROM cities 
+LEFT JOIN states ON states.id = cities.state_id 
+ORDER BY cities.id ASC;
