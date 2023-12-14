5e40bd2651c2ee3f64d3adef782dcba4db544e46
zyadhany <zeyad.hany2003@gmail.com>
2023-12-14 04:17:45 +0200

    added task

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- Get all DB of server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
+CREATE TABLE IF NOT EXISTS cities(
     id INT UNIQUE NOT NULL AUTO_INCREMENT,
     state_id INT NOT null
     name VARCHAR(256) NOT null,
