f935b564e42859ab4b7a9b8e31656e9eba85717e
zyadhany <zeyad.hany2003@gmail.com>
2023-12-14 04:16:54 +0200

    added task

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 -- Get all DB of server.
-CREATE TABLE if NOT EXISTS id_not_null (
+CREATE TABLE if NOT EXISTS unique_id (
     id INT DEFAULT 1 UNIQUE,
     name VARCHAR(256)
 );
--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,2 +1,10 @@
 -- Get all DB of server.
-INSERT INTO first_table (id, name) VALUES (89, "Best School");
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+    id INT UNIQUE NOT NULL AUTO_INCREMENT,
+    state_id INT NOT null
+    name VARCHAR(256) NOT null,
+    FOREIGN KEY(state_id) REFERENCES states(id)),
+    PRIMARY KEY (id)
+);
