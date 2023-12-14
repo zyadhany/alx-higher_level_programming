dbc3a0ef80358f7e6610bf33b622dd5ace25195c
zyadhany <zeyad.hany2003@gmail.com>
2023-12-14 04:45:06 +0200

    added task 9

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,6 +1,6 @@
 -- Get all DB of server.
 
-SELECT name FROM tv_genres WHERE NOT IN (
+SELECT name FROM tv_genres WHERE name NOT IN (
 SELECT name FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
