9eee0067070de8a8ea11f03c08ba988cebced46b
zyadhany <zeyad.hany2003@gmail.com>
2023-12-14 04:43:33 +0200

    added task 9

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,4 +1,9 @@
 -- Get all DB of server.
-USE hbtn_0c_0;
-ALTER TABLE `first_table`
-CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0d_tvshows;
+SELECT name FROM tv_genres WHERE not IN (
+SELECT name FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter')
+GROUP BY name
+ORDER BY name ASC;
