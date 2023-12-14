6cb999be77b5bdc48005fc8c64c85e023fbbdd94
zyadhany <zeyad.hany2003@gmail.com>
2023-12-14 04:51:34 +0200

    added task 9

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -2,8 +2,8 @@
 
 SELECT title FROM tv_shows WHERE title NOT IN (
 SELECT title FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy')
 GROUP BY title
 ORDER BY title ASC;
