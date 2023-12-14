903646e94701dddc78afcb1097b3e065c0b7e9b4
zyadhany <zeyad.hany2003@gmail.com>
2023-12-14 04:35:11 +0200

    added task 9

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,6 +5,6 @@ CREATE TABLE IF NOT EXISTS cities(
     id INT UNIQUE NOT NULL AUTO_INCREMENT,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
     FOREIGN KEY(state_id) REFERENCES states(id))
 );
