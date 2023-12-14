670cc97e0f933004a37899b5e5317591f896bc36
zyadhany <zeyad.hany2003@gmail.com>
2023-12-14 04:21:27 +0200

    added task

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,8 +3,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
     id INT UNIQUE NOT NULL AUTO_INCREMENT,
-    state_id INT NOT null
+    state_id INT NOT null,
     name VARCHAR(256) NOT null,
     FOREIGN KEY(state_id) REFERENCES states(id)),
-    PRIMARY KEY (id)
-);
+    PRIMARY KEY (id));
