d962008196d30688b15cc75dcd9d4a2e497959f7
zyadhany <zeyad.hany2003@gmail.com>
2023-12-14 04:34:06 +0200

    added task 9

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- Get all DB of server.
 SELECT id, name FROM cities WHERE state_id = (
-    SELECT id FROM state WHERE name="California"
+    SELECT id FROM states WHERE name="California"
 ) ORDER BY id ASC;
