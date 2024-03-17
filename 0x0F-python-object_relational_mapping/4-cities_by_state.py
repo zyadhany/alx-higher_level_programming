4ef45fb7070761a036911f89fc73fa13510077e7
zyadhany <zeyad.hany2003@gmail.com>
2024-03-17 15:12:18 +0200

    made #2 task of Object-relational mapping

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""  list all element of table state """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    USER = sys.argv[1]
+    PASS = sys.argv[2]
+    DB = sys.argv[3]
+
+    db = MySQLdb.connect(host='localhost', user=USER,
+                         passwd=PASS, db=DB, port=3306)
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states")
+    state = cur.fetchall()
+    cur.execute("""SELECT cities.id, cities.name, states.name AS state_name
+                    FROM cities
+                    INNER JOIN states ON cities.state_id = states.id;
+                """)
+    results = cur.fetchall()
+
+    for row in results:
+        print(row)
+
+    cur.close()
+    db.close()
