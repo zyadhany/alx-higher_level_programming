9d2c2242c7aebd87867c175e0aa834d81ccd3c69
zyadhany <zeyad.hany2003@gmail.com>
2024-03-17 14:20:51 +0200

    made first task of Object-relational mapping

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+USER = sys.argv[1]
+PASS = sys.argv[2]
+DB = sys.argv[3]
+
+
+db = MySQLdb.connect(host='localhost', user=USER, passwd=PASS, db=DB)
+
+cur = db.cursor()
+
+cur.execute("SELECT * FROM states")
+
+results = cur.fetchall()
+
+for row in results:
+    print(row)
+
+cur.close()
+db.close()
