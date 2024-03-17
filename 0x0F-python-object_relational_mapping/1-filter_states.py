a3d837b02761d7e6ab98d75de037eb335a23cd2b
zyadhany <zeyad.hany2003@gmail.com>
2024-03-17 14:28:46 +0200

    made #2 task of Object-relational mapping

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""  list all element of table state """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    USER = sys.argv[1]
+    PASS = sys.argv[2]
+    DB = sys.argv[3]
+
+    db = MySQLdb.connect(host='localhost', user=USER,
+                         passwd=PASS, db=DB, port=3306)
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states")
+
+    results = cur.fetchall()
+
+    for row in results:
+        if (row[1][0] == 'N'):
+            print(row)
+
+    cur.close()
+    db.close()
