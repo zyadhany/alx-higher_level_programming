13380cbfea9b9ffa6b3690f427abe45d1ba105c2
zyadhany <zeyad.hany2003@gmail.com>
2024-03-31 22:54:42 +0200

    solved task

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Start link class to table in database """
 import sys
-from model_state import Base, State
-from model_city import City
+from relationship_state import Base, State
+from relationship_city import City
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
