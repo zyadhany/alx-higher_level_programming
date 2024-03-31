47fa6ddd8fe5dde1889038b183796377d935fead
zyadhany <zeyad.hany2003@gmail.com>
2024-03-31 22:47:01 +0200

    solved task

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""  state model """
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+
+class State(Base):
+    """
+    class state for db
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
