d50d89a31ab05ac1f40e3e05e4e6d42a47957adb
zyadhany <zeyad.hany2003@gmail.com>
2024-03-31 22:55:57 +0200

    solved task

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """  state model """
-from model_state import Base, State
+from relationship_state import Base, State
 from sqlalchemy import Column, Integer, String, MetaData, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
