edafbcc4b28fd4f0413ecd37daa26516a00c8c20
zyadhany <zeyad.hany2003@gmail.com>
2024-04-01 14:38:21 +0200

    solved task

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,10 +3,27 @@
 ''' get peak '''
 
 
-def find_peak(list_of_integers):
+def find_peak(X):
     ''' get peak func '''
 
-    if not list_of_integers or len(list_of_integers) == 0:
+    if not X or len(X) == 0:
         return (None)
 
-    return max(list_of_integers)
+    l = 0
+    r = len(X) - 1
+
+    while l < r:
+        at = (l + r) // 2
+
+        if X[at] <= X[l]:
+            r = at
+        elif X[at] <= X[r]:
+            l = at
+        elif X[at] >= X[at - 1]:
+            l = at
+        elif X[at] >= X[at + 1]:
+            r = at
+        else:
+            r = at - 1
+
+    return X[l]
